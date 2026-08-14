6de1081bb2037b534b4eaf0db5115d55803a4dec
Vladislav Zainullin <mittoface02@mail.ru>
2026-08-15 00:12:07 +0300

    Добавляет дополнительные дефолтные конструкторы для исключения leptonica

--- Leptonica/LeptonicaException.cs
+++ Leptonica/LeptonicaException.cs
@@ -2,9 +2,18 @@ namespace Leptonica;
 
 public sealed class LeptonicaException : Exception
 {
+    public LeptonicaException()
+    {
+    }
+    
     public LeptonicaException(
         string message)
         : base(message)
     {
+    }
+
+    public LeptonicaException(string message, Exception innerException) : base(message, innerException)
+    {
+        
     }
 }

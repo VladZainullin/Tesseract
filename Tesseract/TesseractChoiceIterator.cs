4c3aa94c61b6822336f51cceb8c8832ffda86cf5
Vladislav Zainullin <mittoface02@mail.ru>
2026-08-15 05:19:53 +0300

    Удаляет Disposed из итератора вариантов

--- Tesseract/TesseractChoiceIterator.cs
+++ Tesseract/TesseractChoiceIterator.cs
@@ -6,7 +6,6 @@ namespace Tesseract;
 public sealed class TesseractChoiceIterator : IDisposable, ITesseractChoiceIterator
 {
     private readonly TesseractChoiceIteratorSafeHandle _handle;
-    private volatile bool _disposed;
 
     public TesseractChoiceIterator(TesseractChoiceIteratorSafeHandle handle)
     {
@@ -15,26 +14,21 @@ public sealed class TesseractChoiceIterator : IDisposable, ITesseractChoiceItera
 
     public bool TryNext()
     {
-        ObjectDisposedException.ThrowIf(_disposed, this);
         return TesseractNative.TessChoiceIteratorNext(_handle);
     }
 
     public string GetText()
     {
-        ObjectDisposedException.ThrowIf(_disposed, this);
         return TesseractNative.TessChoiceIteratorGetUtf8Text(_handle);
     }
 
     public float GetConfidence()
     {
-        ObjectDisposedException.ThrowIf(_disposed, this);
         return TesseractNative.TessChoiceIteratorConfidence(_handle);
     }
 
     public void Dispose()
     {
-        if (_disposed) return;
-        _disposed = true;
         _handle.Dispose();
     }
 }
